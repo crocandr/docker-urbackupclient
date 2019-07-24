d6b6ebad167975c7ad09ed09abcbf1fd9b5ae4b6
croc <slezsak.andras@gmail.com>
2019-07-24 16:23:15 +0200

    os changed to debian and some updates

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM ubuntu:xenial
+FROM debian:9
 
-RUN apt-get update && apt-get install -y vim curl
-RUN curl -L -o /opt/urclient.sh "https://hndl.urbackup.org/Client/2.0.33/UrBackup%20Client%20Linux%202.0.33.sh"
+ENV DOWNLOAD_URL https://hndl.urbackup.org/Client/2.3.4/UrBackup%20Client%20Linux%202.3.4.sh
+
+RUN apt-get update && apt-get install -y curl
+RUN curl -L -o /opt/urclient.sh "$DOWNLOAD_URL"
 
 COPY files/start.sh /opt/start.sh
 RUN chmod 755 /opt/start.sh
 
+ENTRYPOINT /opt/start.sh
--- files/start.sh
+++ files/start.sh
@@ -12,4 +12,20 @@ fi
 # run
 ln -f -s /opt/urclient/x86_64-linux-eng/urbackupclient* /usr/local/bin/
 
-urbackupclientbackend --config /opt/urclient/config --no-consoletime
+
+#urbackupclientbackend --config /opt/urclient/config --no-consoletime
+urbackupclientbackend --config /opt/urclient/config --no-consoletime --daemon
+
+sleep 10
+
+# add source dirs
+for SRC_DIR in $( echo $SOURCES | sed s@\"@@g | sed s@\'@@g )
+do
+  [ ! -z "$SRC_DIR" ] && { echo "adding $SRC_DIR as backup source..."; urbackupclientctl add-backupdir -d $SRC_DIR; }
+  sleep 3
+done
+# list sources
+urbackupclientctl list-backupdir
+
+
+tail -f /var/log/urbackupclient.log
